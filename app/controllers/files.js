4625a2a915f74f13f527597fd42169ee2b90860b
rdelvallej32 <rdelvallej@gmail.com>
2016-05-13 16:01:25 -0400

    Add destroy method
    Destroy method includes the search feature

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -2,10 +2,32 @@
 
 const controller = require('lib/wiring/controller');
 const models = require('app/models');
+const File = models.file;
 
-///Need to require File Model here///
+const authenticate = require('./concerns/authenticate');
+
+
+
+const destroy = (req, res, next) => {
+  let search = { _id: req.params.id, _owner: req.currentUser._id };
+  File.findOne(search)
+    .then(file => {
+      if (!file) {
+        return next();
+      }
+
+      return file.remove()
+        .then(() => res.sendStatus(200));
+    })
+    .catch(err => next(err));
+};
 
 
 module.exports = controller({
+  destroy,
+},
+{ before: [
+  { method: authenticate },
+]
 
  });
