4d188f67366de404c43884cdc238a5c7aab3e47b
rdelvallej32 <rdelvallej@gmail.com>
2016-05-13 15:45:08 -0400

    Require necessary files for controller
    Added module.exports
    Need to require File model Schema still

---
+++ app/controllers/files.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+
+///Need to require File Model here///
+
+
+module.exports = controller({
+
+ });
